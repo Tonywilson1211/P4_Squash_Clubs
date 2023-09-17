ab58ee7cbb0ba2d6b74a7723f5e95f82228537e6
Tonywilson1211 <>
2023-09-17 12:29:16 +0000

    add allowed hosts - django successful installed and tested

--- squash/settings.py
+++ squash/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-561%)b-u0*o0+n_qgsor_)+*!#1j=(6u(jz1p_npr73yjrrm6j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-tonywilson1211-p4-squash-29edgjknqg.us2.codeanyapp.com']
 
 
 # Application definition
